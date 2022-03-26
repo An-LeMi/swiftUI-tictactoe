e96d5db0ee2b5da8e7025e15c008cc9512550f3e
An-LeMi <leminhan18@gmail.com>
2022-03-26 11:01:56 +0700

    Initial Commit

---
+++ tic-tac-toe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  tic-tac-toe
+//
+//  Created by Lê Minh An on 26/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ tic-tac-toe/tic_tac_toeApp.swift
@@ -0,0 +1,17 @@
+//
+//  tic_tac_toeApp.swift
+//  tic-tac-toe
+//
+//  Created by Lê Minh An on 26/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct tic_tac_toeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
